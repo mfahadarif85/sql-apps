1eb56d21b409cda810b0d1e24538b906a189da19
mfahadarif85 <mfahadarif@gmail.com>
2023-03-28 23:30:22 +0500

    recipes ingredients

--- recipes/api.js
+++ recipes/api.js
@@ -20,38 +20,83 @@ router.get("/detail", (_, res) =>
 /**
  * Student code starts here
  */
-
-// connect to postgres
+const pg = require("pg");
+const pool = new pg.Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "recipeguru",
+  password: "lol",
+  port: 5432,
+});
 
 router.get("/search", async function (req, res) {
   console.log("search recipes");
-
-  // return recipe_id, title, and the first photo as url
-  //
-  // for recipes without photos, return url as default.jpg
-
-  res.status(501).json({ status: "not implemented", rows: [] });
+  const { rows } = await pool.query(`
+    SELECT DISTINCT ON (r.recipe_id)
+      r.recipe_id, r.title, COALESCE(rp.url, 'default.jpg') AS url
+    FROM
+      recipes r
+    LEFT JOIN
+      recipes_photos rp
+    ON
+      r.recipe_id = rp.recipe_id;
+  `);
+  res.json({ rows }).end();
 });
 
 router.get("/get", async (req, res) => {
   const recipeId = req.query.id ? +req.query.id : 1;
   console.log("recipe get", recipeId);
 
-  // return all ingredient rows as ingredients
-  //    name the ingredient image `ingredient_image`
-  //    name the ingredient type `ingredient_type`
-  //    name the ingredient title `ingredient_title`
-  //
-  //
-  // return all photo rows as photos
-  //    return the title, body, and url (named the same)
-  //
-  //
-  // return the title as title
-  // return the body as body
-  // if no row[0] has no photo, return it as default.jpg
+  const ingredientsPromise = pool.query(
+    `
+    SELECT
+      i.title AS ingredient_title,
+      i.image AS ingredient_image,
+      i.type AS ingredient_type
+    FROM
+      recipe_ingredients ri
 
-  res.status(501).json({ status: "not implemented" });
+    INNER JOIN
+      ingredients i
+    ON
+      i.id = ri.ingredient_id
+
+    WHERE
+      ri.recipe_id = $1;
+  `,
+    [recipeId]
+  );
+
+  const photosPromise = pool.query(
+    `
+    SELECT 
+      r.title, r.body, COALESCE(rp.url, 'default.jpg') AS url
+    FROM 
+      recipes r
+
+    LEFT JOIN
+      recipes_photos rp
+    ON
+      rp.recipe_id = r.recipe_id
+
+    WHERE 
+      r.recipe_id = $1;
+  `,
+    [recipeId]
+  );
+
+  const [{ rows: photosRows }, { rows: ingredientsRows }] = await Promise.all([
+    photosPromise,
+    ingredientsPromise,
+  ]);
+
+  res.json({
+    ingredients: ingredientsRows,
+    photos: photosRows.map((photo) => photo.url),
+    title: photosRows[0].title,
+    body: photosRows[0].body,
+  });
 });
 /**
  * Student code ends here
